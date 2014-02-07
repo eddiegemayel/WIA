913d9d009c5b96efde4de2aa0f9804a90f85c354
eddiegemayel <eddiegem16@gmail.com>
2014-02-06 20:02:54 -0500

    Basic Page setup

---
+++ project files/js/main.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+	var Canvas1 = document.getElementById("Canvas1");
+
+	if (Canvas1 && Canvas1.getContext) {
+		var ctx = Canvas1.getContext("2d");
+		if(ctx){
+			
+		}
+	}
+}
