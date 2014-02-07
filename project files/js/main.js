5bda5dbbc311c23aed52a674ba55c9d82ae0cce2
eddiegemayel <eddiegem16@gmail.com>
2014-02-06 20:20:32 -0500

    Put the logo on the index using canvas

--- project files/js/main.js
+++ project files/js/main.js
@@ -4,7 +4,8 @@ window.onload = function(){
 	if (Canvas1 && Canvas1.getContext) {
 		var ctx = Canvas1.getContext("2d");
 		if(ctx){
-			
+			var img = document.getElementById("img1");
+			ctx.drawImage(img, 0, 0);
 		}
 	}
 }
