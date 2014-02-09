a76fb6860cf74d719ebc0cf6838bbe544de96761
eddiegemayel <eddiegem16@gmail.com>
2014-02-08 19:20:42 -0500

    index
    very early html build of the Express Pizza Index page

--- project files/js/main.js
+++ project files/js/main.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 	var Canvas1 = document.getElementById("Canvas1");
-
+	//draws logo
 	if (Canvas1 && Canvas1.getContext) {
 		var ctx = Canvas1.getContext("2d");
 		if(ctx){
