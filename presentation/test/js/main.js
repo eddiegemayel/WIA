6ac55b066668450adaff280cc4c6b937ba22f816
eddiegemayel <eddiegem16@gmail.com>
2014-02-11 22:03:56 -0500

    testing new plugin

--- presentation/test/js/main.js
+++ presentation/test/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-     $('.edit').editable('http://www.example.com/save.php');
+     $('.edit').editable('http://www.example.com/save.php', {
+     	indicator : "<img src='img/indicator.gif'>",
+      // type   : 'textarea',
+      // // submitdata: { _method: "put" },
+      // submit : 'OK',
+      // cancel : 'cancel'
+      indicator : "<img src='../../project/images/pep.png'>",
+      tooltip   : "Move mouseover to edit...",
+      event     : "mouseover",
+     });
  });
