ce6ee3b421ef22b161c16fd7a49293783bdbf983
eddiegemayel <eddiegem16@gmail.com>
2014-02-14 21:10:51 -0500

    Presentation Changes

--- presentation/test/js/main.js
+++ presentation/test/js/main.js
@@ -1,38 +1,36 @@
 (function($){
 	$(function() {
+
+		//Initialize the JCarousel
    		 $('.jcarousel').jcarousel({
+   		 	//Wrap around to first or last <li>
    		 	wrap: "both"
    		 });
 
+   		 //Previous Button
         $('.jcarousel-control-prev')
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
+        	//When clicked, move back 1 <li>
             .jcarouselControl({
                 target: '-=1'
             });
 
+          //Next Button
         $('.jcarousel-control-next')
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
+        	//When clicked, move forward one <li>
             .jcarouselControl({
                 target: '+=1'
             });
 
+          //Pagination
         $('.jcarousel-pagination')
+        	//Which dot gets highlighted
             .on('jcarouselpagination:active', 'a', function() {
                 $(this).addClass('active');
             })
             .on('jcarouselpagination:inactive', 'a', function() {
                 $(this).removeClass('active');
             })
+
             .jcarouselPagination();
     });
  })(jQuery);
