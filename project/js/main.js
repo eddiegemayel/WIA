cbce4b4c901f88a74edd828db63923f33d981d10
eddiegemayel <eddiegem16@gmail.com>
2014-02-27 12:15:10 -0500

    Added second use of canvas
    Added canvas to display a small text header on the menu page.

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,24 @@
 window.onload = function(){
 	var Canvas1 = document.getElementById("Canvas1");
+	var Canvas2 = document.getElementById("Canvas2");
 	//draws logo
 	if (Canvas1 && Canvas1.getContext) {
 		var ctx = Canvas1.getContext("2d");
 		if(ctx){
 			var img = document.getElementById("img1");
 			ctx.drawImage(img, 0, 0);
+		}
+	}
+
+	//Draws a level 6 header text
+	if (Canvas2 && Canvas2.getContext) {
+		var ctx = Canvas2.getContext("2d");
+		if(ctx){
+			var string = "Choose A Tasty Item!";
+
+			ctx.font = "bold 1.15rem Helvetica Neue";
+			ctx.fillStyle = "#161212";
+			ctx.fillText(string, 0, 15);
 		}
 	}
 
